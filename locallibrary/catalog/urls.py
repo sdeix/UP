a036a56e20ee4c5a1f086652293560433e78e3bd
Сергей Малюгов <gr402_maspe@lab-402-5.tomtit-tomsk.ru>
2022-09-24 10:03:58 +0700

    first commit

---
+++ locallibrary/catalog/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+
+
+from django.urls import include, re_path
+
+urlpatterns = [
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+]
+
+urlpatterns += [
+    re_path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+]
+
+urlpatterns += [
+    re_path(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+urlpatterns += [
+    re_path(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    re_path(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    re_path(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
+]
